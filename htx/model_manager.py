7020faae7e3b09163442bd0b5e2498b32751c054
nik <nik@heartex.net>
2019-05-27 12:44:03 +0300

    setup logging & cached model resources

--- htx/model_manager.py
+++ htx/model_manager.py
@@ -107,18 +107,34 @@ class ModelManager(object):
         logger.info(f'Project {project}: latest train job found: {latest_job}')
         return latest_job
 
+    def _stash_resources(self, project, resources):
+        self._redis.set(f'project:{project}:res', resources)
+
+    def _try_stashpop_resources(self, project):
+        return self._redis.get(f'project:{project}:res')
+
     def setup(self, project, schema):
         train_job = self._get_latest_finished_train_job(project)
-        if not train_job:
-            logger.info('No one training job has been finished yet.')
+        if train_job:
+            result = train_job.result
+        else:
+            # in case when jobs are broken, try to stash pop train resources from cache
+            result = self._try_stashpop_resources(project)
+
+        if not result:
+            logger.info('Couldn\'t load train resources neither from latest jobs, nor from cache. Model is not loaded, '
+                        'and consequent API calls (e.g. predict) will fail. This normally happens if your model '
+                        'training is not started at very beginning. Otherwise it is a bug.')
             return None
 
         model = self.create_model_func(**schema)
-        model_version = model.load(train_job.result)
+        model_version = model.load(result)
         if model_version is None:
-            logger.error('Model is not loaded.')
+            logger.error(f'Found resources {result}, but model is not loaded. Consequent API calls (e.g. predict)'
+                         f' will fail.')
             return None
         self._current_model[project] = model
+        logger.info(f'Model {model_version} successfully loaded for project {project}.')
         return model_version
 
     def validate(self, config):
